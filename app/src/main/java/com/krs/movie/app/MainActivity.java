fb73bd0e81e2eaf1fb6ae27aabb36bc6314be3ac
kasunSomadasa <kasunsomadasa94@gmail.com>
2022-12-06 16:14:58 +0530

    initial commit

---
+++ app/src/main/java/com/krs/movie/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.krs.movie.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
